230bd7627f7ba002869b36832c02e10e7cf53378
Cristian Felix Iñiguez Porcel <felizpc1@gmail.com>
2020-08-13 14:59:40 -0400

    Configurar CSS para administrar elementos visuales

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { resolve } = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
@@ -27,6 +27,12 @@ module.exports = {
             inject: true,
             template: './public/index.html',
             filename: './index.html'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: './src/styles/styles.css',
+                to: ''
+            }],
         })
     ]
 }
